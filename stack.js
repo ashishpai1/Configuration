0217ee546d3cc96b796c63bf1fc09e5722dd50ef
Ashish Pai <ashish.pai.git@gmail.com>
2016-05-13 00:30:40 -0700

    Update stack.js

--- stack.js
+++ stack.js
@@ -4,15 +4,19 @@
 function isValid(myStr){
     var arr = myStr.trim().replace(/ /g,"").split("");
     var stack = [];
+    if(arr.length % 2 !== 0){
+        return false;
+    }
     for(var i=0;i< arr.length;i++){
          console.log(stack)
          var currentElement = arr[i];
          if(currentElement === "[" || currentElement === "("  || currentElement === "{" ){
                         stack.push(currentElement);
          }else{
-             if(currentElement === "]" || currentElement === ")"  || currentElement === "}" ){
-                      var poppedElement = stack.pop();
-             }   
+                var poppedElement = stack.pop();
+                if(poppedElement === ")" || poppedElement === "}" ||  poppedElement === "]"){
+                    return false; 
+                }
          }        
     }
     if(stack.length === 0){
